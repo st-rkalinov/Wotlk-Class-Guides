887c366aff3cdd7e0a76692b35001ec59c6e09e0
Stoyan Kalinov <bloodyrax@gmail.com>
2020-06-26 17:38:26 +0300

    add changes for production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+    apiKey: 'AIzaSyC3xiUpDUN3fpJouH21JaRiRHOtRM8o6Fo',
+    authDomain: 'wow-class-guides.firebaseapp.com',
+    databaseURL: 'https://wow-class-guides.firebaseio.com',
+    projectId: 'wow-class-guides',
+    storageBucket: 'wow-class-guides.appspot.com',
+    messagingSenderId: '721607600306',
+    appId: '1:721607600306:web:01f68b9671d68e4099c1ab'
+  }
 };
